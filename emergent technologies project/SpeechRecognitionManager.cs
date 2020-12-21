32e8467323537788cd3314eff1aee4f2f566dbf7
dkc1011 <t00201097@students.ittralee.ie>
2020-12-21 22:19:19 +0000

    Uploaded to Git
    Signed-off-by: dkc1011 <t00201097@students.ittralee.ie>

---
+++ emergent technologies project/SpeechRecognitionManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class SpeechRecognitionController1 : MonoBehaviour
+{
+    private KeywordRecognizer recEngine;
+    private Dictionary<string, Action> Commands = new Dictionary<string, Action>();
+
+    private void Start()
+    {
+        Commands.Add("forward", Forward);
+        Commands.Add("back", Back);
+        Commands.Add("up", Up);
+        Commands.Add("down", Down);
+
+        recEngine = new KeywordRecognizer(Commands.Keys.ToArray());
+        recEngine.OnPhraseRecognized += SpeechRecognized;
+        recEngine.Start();
+    }
+
+    private void SpeechRecognized(PhraseRecognizedEventArgs speech)
+    {
+        Debug.Log(speech.text);
+        Commands[speech.text].Invoke();
+    }
+
+    private void Forward()
+    {
+        transform.Translate(1, 0, 0);
+    }
+
+    private void Back()
+    {
+        transform.Translate(-1, 0, 0);
+    }
+
+    private void Up()
+    {
+        transform.Translate(0, 1, 0);
+    }
+
+    private void Down()
+    {
+        transform.Translate(0, -1, 0);
+    }
+}
